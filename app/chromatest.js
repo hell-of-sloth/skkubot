82846174cd0c1438e21b5a5c8da054121be59d09
caboodles <121304832+caboodles@users.noreply.github.com>
2024-01-31 20:47:40 +0900

    24 1 ai in devcontainer test (#28)
    * feat: ai in devcontainer
    
    * fix:fixed port problem and OpenAI key leak
    
    * docs: clarified readme
    
    * hotfix: merge main
    
    ---------
    
    Co-authored-by: Gyunseo Lee <rbstj0311@naver.com>

---
+++ app/chromatest.js
@@ -0,0 +1,32 @@
+const ChatOpenAI = require("@langchain/openai").ChatOpenAI;
+const OpenAIEmbeddings = require("@langchain/openai").OpenAIEmbeddings;
+const { PromptTemplate } = require("@langchain/core/prompts");
+const {
+  RunnableSequence,
+  RunnablePassthrough,
+} = require("@langchain/core/runnables");
+const { StringOutputParser } = require("@langchain/core/output_parsers");
+const { Chroma } = require("@langchain/community/vectorstores/chroma");
+const { ChromaClient } = require("chromadb");
+const { TextLoader } = require("langchain/document_loaders/fs/text");
+
+const client = new ChromaClient({
+  path: "http://chroma:8000",
+});
+
+async function findcollections() {
+  const collections = await client.listCollections();
+  console.log(collections);
+}
+
+findcollections();
+async function readdb() {
+  const collection = await client.getCollection({
+    name: "skkubot",
+  });
+  const results = await collection.peek({
+    limit: 10,
+  });
+  console.log(results);
+}
+readdb();
