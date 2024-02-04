514b567789059ec37e28ef348836adcbaf2cb134
SJY <jun000405@naver.com>
2024-02-04 10:07:04 +0000

    feat: nestjs and typescript migration comlplete
    - 일단 작동함
    - 프론트쪽은 빼놨음
    - 일부 URL 변경했으니 참고 바람
    -- http://localhost:3000/chatbot/addDB
    -- http://localhost:3000/chatbot/question
    -- http://localhost:3000/chatbot/resetDB
    
    - 추후 가다듬을 예정
    - 몇몇 패키지 추가 (@nestjs/config, class-validator 등등)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ChatbotModule } from './chatbot/chatbot.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ChatbotModule, AuthModule],
+  imports: [
+    ChatbotModule, 
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- backend/src/chatbot/chatbot.controller.ts
+++ backend/src/chatbot/chatbot.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post } from '@nestjs/common';
+import { ChatbotService } from './chatbot.service';
+import { addDBDto } from './dto/addDB.dto';
+import { questionDto } from './dto/question.dto';
 
 @Controller('chatbot')
-export class ChatbotController {}
+export class ChatbotController {
+    constructor(private chatbotService: ChatbotService) { }
+
+    @Get()
+    getChatbot(): void {
+        // 프론트 엔드 출력용
+    }
+
+    @Post('addDB')
+    postVectorDB(@Body() adddbdto: addDBDto): Promise<void> {
+        const { filename } = adddbdto;
+        console.log(filename);
+        return this.chatbotService.startvectordb(filename);
+    }
+
+    @Post('question')
+    postChatbot(@Body() questiondto: questionDto): Promise<string> {
+        const { question } = questiondto;
+        console.log(question);
+        return this.chatbotService.askAI(question);
+    }
+
+    @Delete('resetDB')
+    deleteResetDB(): Promise<void> {
+        return this.chatbotService.resetDB();
+    }
+}
