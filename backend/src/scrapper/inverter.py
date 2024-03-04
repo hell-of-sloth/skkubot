69d13cc18357532b4592a941f45a828e672566b7
SJY <jun000405@naver.com>
2024-03-04 06:08:58 +0000

    fix: scrapper hwp format issue
    - 이제 모든 페이지를 불러옴
    - 뭔 이상한 한자랑 문자 아닌게 섞이는데 참고한 글에서도 같은 이슈, 해결하려고 했지만 실패
    - 참고
    https://blog.arecacoco.com/entry/%ED%95%9C%EA%B8%80-hwp-%ED%8C%8C%EC%9D%BC%EC%9D%84-%ED%85%8D%EC%8A%A4%ED%8A%B8-txt-%EB%A1%9C-%EA%B8%81%EC%96%B4%EC%99%80%EC%84%9C-%EB%8D%B0%EC%9D%B4%ED%84%B0%EB%A1%9C-%ED%99%9C%EC%9A%A9%ED%95%98%EA%B8%B0

--- backend/src/scrapper/inverter.py
+++ backend/src/scrapper/inverter.py
@@ -3,7 +3,8 @@ import os
 import fitz # PyMuPDF 패키지 설치 필요
 import docx # python-docx 패키지 설치 필요
 from pptx import Presentation # python-pptx 패키지 설치 필요
-import subprocess
+import zlib # zlib 패키지 설치 필요
+import struct
 
 
 def fileInverter():
@@ -29,9 +30,52 @@ def fileInverter():
                 txt_f = open(txt_path + "/" + file + ".txt", 'w', encoding='UTF-8')
                 filename = os.path.join(folder_path, file)
                 f = olefile.OleFileIO(filename)
-                encoded_text = f.openstream('PrvText').read()
-                decoded_text = encoded_text.decode('UTF-16')
-                txt_f.write(decoded_text)
+                dirs = f.listdir()
+                
+                # 문서 포맷 압축 여부 확인
+                header = f.openstream("FileHeader")
+                header_data = header.read()
+                is_compressed = (header_data[36] & 1) == 1
+                
+                # Body section 추출
+                nums = []
+                for d in dirs:
+                    if d[0] == "BodyText":
+                        nums.append(int(d[1][len("Section"):]))
+                sections = ["BodyText/Section" + str(i) for i in sorted(nums)]
+                
+                # text 추출
+                
+                hwp_text = ""
+                for section in sections:
+                    bodytext = f.openstream(section)
+                    hwp_data = bodytext.read()
+                    if is_compressed:
+                        unpacked_data = zlib.decompress(hwp_data, -15)
+                    else:
+                        unpacked_data = hwp_data
+                        
+                    # 각 section의 text 추출
+                    
+                    section_text = ""
+                    i = 0
+                    size = len(unpacked_data)
+                    while i < size:
+                        header = struct.unpack_from("<l", unpacked_data, i)[0]
+                        rec_type = header & 0x3ff
+                        rec_len = (header >> 20) & 0xfff
+                        
+                        if rec_type in [67]:
+                            rec_data = unpacked_data[i + 4:i + 4 + rec_len]
+                            section_text += rec_data.decode("utf-16")
+                            section_text += "\n"
+                    
+                        i += 4 + rec_len
+                        
+                    hwp_text += section_text
+                    hwp_text += "\n"
+                                    
+                txt_f.write(hwp_text)
                 txt_f.close()
                 
             # PDF 파일 .pdf
